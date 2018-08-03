7bf3693355817dee67713159f5d699ac676b1c7a
Ling Yang <shauroshanying@gmail.com>
2018-08-02 21:06:06 -0400

    update debug tool

--- lab/src/raft/go_test.sh
+++ lab/src/raft/go_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 for i in `seq $1`; do
-  echo "Running test..."
+  echo "Running test $i..."
   go test -run 2A >> test_result.log
 done
 success=$(grep -E '^PASS$' test_result.log | wc -l)
